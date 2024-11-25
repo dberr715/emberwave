c649bfe30681e34965386dcd17328f8377cb3c19
David Berry <dberry3755@gmail.com>
2024-11-25 12:30:17 -0500

    testing change

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="relative h-full">
       {/* Content */}
       <div className="relative z-10 flex h-screen flex-col items-center justify-center text-center text-white">
-        <h1 className="text-6xl font-bold">Welcome to the Home Page!</h1>
+        <h1 className="text-6xl font-bold">1Welcome to the Home Page!</h1>
         <p className="mt-4 text-xl">Your content goes here.</p>
       </div>
     </div>
