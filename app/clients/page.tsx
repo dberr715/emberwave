4e446ad489b69a06f77fb6f2f07d5b00d49d75dd
David Berry <dberry3755@gmail.com>
2024-11-30 14:38:55 -0500

    added current client layout

--- app/clients/page.tsx
+++ app/clients/page.tsx
@@ -8,15 +8,15 @@ const Clients = () => {
     {
       text: `If you need an AI project done quickly and well by a 'no muss no fuss' guy, talk to David Berry. David listened to my ideas for a SalesGPT product that I had been wanting done for over a year, and in a weekend we had a functioning product.`,
       author: "John Sterling",
-      position: "CEO/Owner/Author, Foxfire Software and Sterling Sales",
+      position: "CEO/Owner/Author, Foxfire Software, and Sterling Sales",
     },
     {
-      text: `I'm thrilled to recommend David and EmberWave, who brings a unique blend of teaching experience and technical expertise to his work in software development. David's work ethic is truly commendable—he embraces hard work, isn't hesitant to seek clarification, and readily extends a helping hand. In the face of progressively more demanding challenges, he consistently rises to the occasion, showcasing a remarkable ability to break down complex problems into manageable chunks.`,
+      text: `I'm thrilled to recommend David and EmberWave, who brings a unique blend of teaching experience and technical expertise to his work in software development. David's work ethic is truly commendable—he embraces hard work, isn't hesitant to seek clarification, and readily extends a helping hand.`,
       author: "Sean Reid",
       position: "Owner/Teacher, TorchCodeLab",
     },
     {
-      text: `During David's time working with us, I was impressed with his rapid learning, strong analytical skills, and effective communication. His ability to draw from his teaching background to foster collaborative and positive learning environments was particularly notable. David's dedication to thoroughly understanding projects and supporting those around him was evident throughout.`,
+      text: `During David's time working with us, I was impressed with his rapid learning, strong analytical skills, and effective communication.`,
       author: "Patisela Alegria",
       position: "Program Coordinator, Carolina Code School",
     },
@@ -24,32 +24,32 @@ const Clients = () => {
 
   const projects = [
     {
-      title: "Project A",
-      description: "Description of Project A.",
-      buttonText: "Learn More",
-      image:
-        "https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp",
+      title: "The Children's Center of Asheboro",
+      description:
+        "The Children's Center of Asheboro never previously had an online presence. They wanted to establish a website to increase community awareness and ensure that new families moving into the area would easily find and learn about their services. Built using WordPress to provide a user-friendly, scalable solution that allowed them to effectively showcase their offerings and engage with the local community.",
+      buttonText: "Website",
+      image: "images/Tree.jpg",
     },
     {
-      title: "Project B",
-      description: "Description of Project B.",
-      buttonText: "Learn More",
-      image:
-        "https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp",
+      title: "Quad AI Lead Generator",
+      description:
+        "Built for a supply chain logistics company, Quad AI is an expert of warehouse logistics for the customer to use. Quad uses AI, uploaded knowledge files, and directives to discover the user's warehouse issues and offer potential solutions. On the business side, administrators have access to chat data and user emails for potential warm lead generation.",
+      buttonText: "Website",
+      image: "images/quadspaceai.png",
     },
     {
-      title: "Project C",
-      description: "Description of Project C.",
-      buttonText: "Learn More",
-      image:
-        "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+      title: "FootyMatch",
+      description:
+        "Leveraging artificial intelligence, FootyMatch aligns users' preferred football, basketball, baseball, and other sports teams with a corresponding soccer team. This enables users to seamlessly immerse themselves in the realm of soccer.",
+      buttonText: "Video Tour",
+      image: "images/footymatch.jpeg",
     },
     {
-      title: "Project D",
-      description: "Description of Project D.",
-      buttonText: "Learn More",
-      image:
-        "https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp",
+      title: "Custom AI GPTs",
+      description:
+        "Crafted numerous custom GPTs tailored to the needs of local business owners, with the goal of expanding their customer reach and enhancing user interactions. Examples include BizFinder, SalesPathFinder, and Sortation, all of which can be found on the GPT store or Chipp.ai marketplace.",
+      buttonText: "Website",
+      image: "images/pathfinder.png",
     },
   ];
 
@@ -59,8 +59,37 @@ const Clients = () => {
   };
 
   return (
-    <div className="container mx-auto px-6 sm:px-12 lg:px-20 py-16 space-y-16 pt-36">
-      {/* Main Section */}
+    <div className="container mx-auto px-6 sm:px-12 lg:px-20 py-10 space-y-6 pt-16">
+      <h2 className="text-4xl font-bold text-highlight mb-8 text-center">
+        Clients
+      </h2>
+      {/* Horizontal Bar Section */}
+      <div className="bg-dark/70 rounded-lg py-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
+          {/* Check Project Status */}
+          <div className="text-center">
+            <h2 className="text-lg font-semibold text-highlight">
+              Active Clients
+            </h2>
+            <button className="bg-highlight text-white font-semibold px-4 py-2 rounded-lg shadow hover:bg-highlight-dark transition">
+              Track your Project
+            </button>
+          </div>
+
+          {/* Contact Us Section */}
+          <div className="text-center">
+            <h2 className="text-lg font-semibold text-highlight">Contact Us</h2>
+            <p className="text-white text-md">dberry3755@gmail.com</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Title for Client Reviews */}
+      {/* <div className="text-center py-4">
+        <h2 className="text-4xl font-bold text-highlight">Client Reviews</h2>
+      </div> */}
+
+      {/* Existing Content */}
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
         {/* Image Section */}
         <div className="relative h-[500px]">
@@ -76,25 +105,15 @@ const Clients = () => {
             {/* Overlay Text at the Top */}
             <div className="absolute top-0 left-0 right-0 flex items-center justify-start bg-gradient-to-b from-black/70 to-transparent py-4 px-6 rounded-t-lg">
               <p className="text-white text-lg lg:text-4xl font-large text-left">
-                Real stories of success and innovation from the businesses and
-                people we've worked with.
+                We value your feedback.
               </p>
             </div>
 
             {/* Become a Satisfied Customer Section */}
             <div className="absolute bottom-0 right-0 flex flex-col items-end space-y-2 bg-gradient-to-t from-black/70 to-transparent py-4 px-6 rounded-b-lg">
-              <h3 className="text-white text-lg lg:text-2xl font-bold text-right">
-                Become a satisfied customer
-              </h3>
               <button className="bg-highlight text-white font-semibold px-4 py-2 rounded-lg shadow hover:bg-highlight-dark transition">
-                Join Now
+                Write a Review
               </button>
-              <a
-                href="#"
-                className="text-white text-sm underline hover:text-highlight-dark mt-2 text-right"
-              >
-                Write a review
-              </a>
             </div>
           </div>
         </div>
@@ -123,7 +142,7 @@ const Clients = () => {
       </div>
 
       {/* Down Arrow */}
-      <div className="flex justify-center ">
+      <div className="flex justify-center py-6 ">
         <button
           className="text-highlight animate-bounce"
           onClick={scrollToProjects}
@@ -132,145 +151,42 @@ const Clients = () => {
         </button>
       </div>
 
-      {/* Completed Projects Section */}
-      <div id="projects" className="py-16">
+      {/* Example Projects Section */}
+      <div id="projects" className="pb-8">
         <h2 className="text-4xl font-bold text-highlight mb-8 text-center">
-          Completed Projects
+          Example Projects
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          {/* The Children's Center of Asheboro */}
-          <div className="card bg-base-100 shadow-xl flex flex-row items-stretch">
-            <figure className="w-2/5">
-              <img
-                src="images/Tree.jpg"
-                alt="The Children's Center of Asheboro"
-                className="object-cover w-full h-full"
-              />
-            </figure>
-            <div className="card-body flex-1 p-6">
-              <h2 className="card-title text-2xl font-semibold">
-                The Children's Center of Asheboro
-              </h2>
-              <p className="text-white-600 text-md">
-                The Children's Center of Asheboro never previously had an online
-                presence. They wanted to establish a website to increase
-                community awareness and ensure that new families moving into the
-                area would easily find and learn about their services. Built
-                using WordPress to provide a user-friendly, scalable solution
-                that allowed them to effectively showcase their offerings and
-                engage with the local community.
-              </p>
-              <div className="card-actions justify-end mt-4">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="btn btn-primary"
-                >
-                  Website
-                </a>
+          {projects.map((project, index) => (
+            <div
+              key={index}
+              className="card bg-base-100 shadow-xl flex flex-row items-stretch"
+            >
+              <figure className="w-2/5">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="object-contain w-full h-full"
+                />
+              </figure>
+              <div className="card-body flex-1 p-6">
+                <h2 className="card-title text-2xl font-semibold">
+                  {project.title}
+                </h2>
+                <p className="text-white-600 text-md">{project.description}</p>
+                <div className="card-actions justify-end mt-4">
+                  <a
+                    href="#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-primary"
+                  >
+                    {project.buttonText}
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-
-          {/* Quad AI Lead Generator */}
-          <div className="card bg-base-100 shadow-xl flex flex-row items-stretch">
-            <figure className="w-2/5">
-              <img
-                src="images/quadspaceai.png"
-                alt="Quad AI Lead Generator"
-                className="object-cover w-full h-full"
-              />
-            </figure>
-            <div className="card-body flex-1 p-6">
-              <h2 className="card-title text-2xl font-semibold">
-                Quad AI Lead Generator
-              </h2>
-              <p className="text-white-600 text-md">
-                Built for a supply chain logistics company, Quad AI is an expert
-                of warehouse logistics for the customer to use. Quad uses AI,
-                uploaded knowledge files, and directives to discover the user's
-                warehouse issues and offer potential solutions. On the business
-                side, administrators have access to chat data and user emails
-                for potential warm lead generation.
-              </p>
-              <div className="card-actions justify-end mt-4">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="btn btn-primary"
-                >
-                  Website
-                </a>
-              </div>
-            </div>
-          </div>
-
-          {/* FootyMatch */}
-          <div className="card bg-base-100 shadow-xl flex flex-row items-stretch">
-            <figure className="w-2/5">
-              <img
-                src="images/footymatch.jpeg"
-                alt="FootyMatch"
-                className="object-cover w-full h-full"
-              />
-            </figure>
-            <div className="card-body flex-1 p-6">
-              <h2 className="card-title text-2xl font-semibold">FootyMatch</h2>
-              <p className="text-white-600 text-md">
-                Leveraging artificial intelligence, FootyMatch aligns users'
-                preferred football, basketball, baseball, and other sports teams
-                with a corresponding soccer team, drawing parallels in terms of
-                historical performance, playing style, and various other
-                criteria. This enables users to seamlessly immerse themselves in
-                the realm of soccer.
-              </p>
-              <div className="card-actions justify-end mt-4">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="btn btn-primary"
-                >
-                  Video Tour
-                </a>
-              </div>
-            </div>
-          </div>
-
-          {/* Custom AI GPTs */}
-          <div className="card bg-base-100 shadow-xl flex flex-row items-stretch">
-            <figure className="w-2/5">
-              <img
-                src="images/pathfinder.png"
-                alt="Custom AI GPTs"
-                className="object-cover w-full h-full"
-              />
-            </figure>
-            <div className="card-body flex-1 p-6">
-              <h2 className="card-title text-2xl font-semibold">
-                Custom AI GPTs
-              </h2>
-              <p className="text-white-600 text-md">
-                Crafted numerous custom GPTs tailored to the needs of local
-                business owners, with the goal of expanding their customer reach
-                and enhancing user interactions. Examples include BizFinder,
-                SalesPathFinder, and Sortation, all of which can be found on the
-                GPT store or Chipp.ai marketplace.
-              </p>
-              <div className="card-actions justify-end mt-4">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="btn btn-primary"
-                >
-                  Website
-                </a>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
