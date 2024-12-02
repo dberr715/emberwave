55fd7b8b11d095e4d7442ac43da43d7a7097adda
David Berry <dberry3755@gmail.com>
2024-12-02 15:20:46 -0500

    made Emberwave in nav bar clickable route to home

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -47,15 +47,17 @@ export default function Navbar() {
                 </Link>
 
                 {/* EmberWave Text - Responsive Alignment */}
-                <span
-                  className={classNames(
-                    "text-white/70 text-2xl tracking-widest uppercase font-bold",
-                    "absolute left-1/2 transform -translate-x-1/2", // Centered by default (mobile)
-                    "sm:relative sm:translate-x-0 sm:left-0 sm:ml-2" // Left-aligned on desktop
-                  )}
-                >
-                  EmberWave
-                </span>
+                <Link href="/" className="flex items-center">
+                  <span
+                    className={classNames(
+                      "text-white/70 text-2xl tracking-widest uppercase font-bold",
+                      "absolute left-1/2 transform -translate-x-1/2", // Centered by default (mobile)
+                      "sm:relative sm:translate-x-0 sm:left-0 sm:ml-2" // Left-aligned on desktop
+                    )}
+                  >
+                    EmberWave
+                  </span>
+                </Link>
               </div>
 
               {/* Navigation Links (Right-Aligned) */}
