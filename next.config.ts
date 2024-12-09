d0563e1f60c6bac64119e7ab68387a78f0909110
David Berry <dberry3755@gmail.com>
2024-12-09 09:50:45 -0500

    updated next config file to ignore

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import withVideos from "next-videos";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* other config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint during build (if needed)
+  },
+  // other config options here
 };
 
 export default withVideos(nextConfig);
