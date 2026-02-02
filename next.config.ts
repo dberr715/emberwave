6daa972117a0bd40fe99e6f550ab01d221d360d8
David Berry <dberry3755@gmail.com>
2026-02-02 11:38:05 -0500

    root directory change for package file

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import withVideos from "next-videos";
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
+  outputFileTracingRoot: path.join(__dirname, "./"),
   eslint: {
     ignoreDuringBuilds: true, // Ignores ESLint during build (if needed)
   },
-  // other config options here
 };
 
 export default withVideos(nextConfig);
